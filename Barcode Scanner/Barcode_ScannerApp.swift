b7951fc082f8df029ae64b2aefc7bc20801ed768
Berkin KOCA <bekoca@innova.com.tr>
2024-03-13 00:29:47 +0300

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Berkin KOCA on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
