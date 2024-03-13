5c573d4689d84de9b2a4d78a62a35a35b668272f
Berkin KOCA <bekoca@innova.com.tr>
2024-03-13 11:38:59 +0300

    error handling and showing Alert is done.

--- Barcode Scanner/BarcodeScannerView.swift
+++ Barcode Scanner/BarcodeScannerView.swift
@@ -7,14 +7,34 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable {
+    let id = UUID()
+    
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+    
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with the camera, We are unable to capture input",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scanned Type",
+                                              message: "The value is scanned is not valid. This app scans EAN-8 and EAN-13.",
+                                              dismissButton: .default(Text("OK")))
+}
+
 struct BarcodeScannerView: View {
     
     @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         NavigationStack {
             VStack {
-                ScannerView(scannedCode: $scannedCode)
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                     .padding(.bottom, 30)
                 
@@ -28,6 +48,11 @@ struct BarcodeScannerView: View {
                 
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
